ab4b8fa0460946ef3683707d6eb6685aefc44f29
nirmiller <nir.miller928@gmail.com>
2023-02-12 22:08:31 -0600

    Animation walking added

--- Hell's-Gym-Fight-Project/player.gd
+++ Hell's-Gym-Fight-Project/player.gd
@@ -33,4 +33,30 @@ func move():
 		$AnimatedSprite.play()
 	
 		
+
+
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("gym_bro_move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("gym_bro_move_left"):
+		velocity.x -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
 	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment.
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.animation = "standing"
+
